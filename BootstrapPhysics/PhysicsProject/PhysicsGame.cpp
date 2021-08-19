03aa9612b8f88d475c51abce95d7088b319bb39c
Ben Brownlie <benjibspam@gmail.com>
2021-08-19 10:15:06 -0500

    Added the scene to PhysicsGame

--- BootstrapPhysics/PhysicsProject/PhysicsGame.cpp
+++ BootstrapPhysics/PhysicsProject/PhysicsGame.cpp
@@ -6,10 +6,13 @@
 
 bool PhysicsGame::startup()
 {
-
+	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
 
 	m_renderer = new aie::Renderer2D();
 	setBackgroundColour(0.0f, 0.2f, 0.1f, 1.0f);
+
+	m_scene = new PhysicsScene();
+	m_scene->setTimeStep(0.01f);
 
 	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
 
@@ -19,6 +22,7 @@ bool PhysicsGame::startup()
 void PhysicsGame::shutdown()
 {
 	delete m_renderer;
+	delete m_scene;
 	delete m_font;
 }
 
@@ -26,6 +30,10 @@ void PhysicsGame::update(float deltaTime)
 {
 	//Get the input instance
 	aie::Input* input = aie::Input::getInstance();
+
+	aie::Gizmos::clear();
+
+	m_scene->update(deltaTime);
 
 	//Exit on Esc
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
@@ -39,6 +47,20 @@ void PhysicsGame::draw()
 	clearScreen();
 
 	m_renderer->begin();
+
+	//Draw the scene
+	m_scene->draw();
+
+	//Draw the Gizmos
+	static float aspectRatio = 16.0f / 9.0f;
+	aie::Gizmos::draw2D(glm::ortho<float>(
+		-100,				//Left
+		 100,				//Right
+		-100 / aspectRatio, //Bottom
+		 100 / aspectRatio, //Top
+		-1.0f,				//zNear
+		 1.0f				//zFar
+		));
 
 	//Draw fps
 	char fps[32];
