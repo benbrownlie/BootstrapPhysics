be5d9ad588df4cab3c91cb79689722eb9039d92a
Ben Brownlie <benjibspam@gmail.com>
2021-08-24 16:54:03 -0500

    Added improved collision resolution to sphere-to-plane

--- BootstrapPhysics/PhysicsProject/PhysicsGame.cpp
+++ BootstrapPhysics/PhysicsProject/PhysicsGame.cpp
@@ -17,11 +17,11 @@ bool PhysicsGame::startup()
 
 	m_scene = new PhysicsScene();
 	m_scene->setTimeStep(0.01f);
-	m_scene->setGravity({ 0.0f, 0.0f });
+	m_scene->setGravity({ 0.0f, -10.0f });
 
 	Sphere* ball1 = new Sphere(glm::vec2(-20.0f, 0), glm::vec2(), 1, 5, glm::vec4(1.0f, 0.5f, 0.2f, 1.0f));
 	m_scene->addActor(ball1);
-	ball1->applyForce(glm::vec2(5.0f, 0.0f));
+	//ball1->applyForce(glm::vec2(5.0f, 0.0f));
 
 	Sphere* ball2 = new Sphere(glm::vec2(20.0f, 0), glm::vec2(), 1, 5, glm::vec4(0.5f, 0.5f, 0.5f, 1.0f));
 	m_scene->addActor(ball2);
