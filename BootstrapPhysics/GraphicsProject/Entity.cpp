14b0813a4fac2f93da1765d95dfe84a18f95fd07
Ben Brownlie <benjibspam@gmail.com>
2021-09-30 16:58:52 -0500

    Began on Entity class

---
+++ BootstrapPhysics/GraphicsProject/Entity.cpp
@@ -0,0 +1,21 @@
+#include "Entity.h"
+
+void Entity::start()
+{
+
+}
+
+void Entity::update()
+{
+
+}
+
+void Entity::draw()
+{
+
+}
+
+void Entity::end()
+{
+
+}
---
+++ BootstrapPhysics/GraphicsProject/Entity.h
@@ -0,0 +1,16 @@
+#pragma once
+class Entity
+{
+public:
+	Entity() {}
+	~Entity() {}
+
+	void start();
+	void update();
+	void draw();
+	void end();
+
+private:
+
+};
+
