1f0ac81f1583f4f8c20aece70cf5a7fa69fb6160
Benjamin Brownlie <benjibspam@gmail.com>
2021-10-02 16:11:38 -0500

    Added changes missed from friday

--- BootstrapPhysics/GraphicsProject/Mesh.cpp
+++ BootstrapPhysics/GraphicsProject/Mesh.cpp
@@ -34,29 +34,16 @@ void Mesh::start()
 	glBindVertexArray(m_vertexArrayObject);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBufferObject);
 
-	//Define the vertices for a quad
-	Vertex vertices[6];
-
-	//triangle 0
-	vertices[0].position = { -0.5f, 0.0f, 0.5f, 1.0f };
-	vertices[1].position = { 0.5, 0.0f, 0.5f, 1.0f };
-	vertices[2].position = { -0.5, 0.0f, -0.5f, 1.0f };
-	//triangle 1
-	vertices[3].position = { 0.5, 0.0f, 0.5f, 1.0f };
-	vertices[4].position = { -0.5, 0.0f, -0.5f, 1.0f };
-	vertices[5].position = { 0.5, 0.0f, -0.5f, 1.0f };
-
 	//Generate the vertices
-	/*Vertex* vertices;
-	int vertexCount;
-	generateVertices(vertices, vertexCount);*/
+	unsigned int vertexCount;
+	Vertex* vertices = generateVertices(vertexCount, m_triCount);
 
 	//Fill vertex buffer
 	glBufferData(
-		GL_ARRAY_BUFFER,	//Type of buffer
-		sizeof(Vertex) * 6, //Size in bytes of all vertices
-		vertices,			//all vertices
-		GL_STATIC_DRAW		//how the data will update
+		GL_ARRAY_BUFFER,				//Type of buffer
+		sizeof(Vertex) * vertexCount,	//Size in bytes of all vertices
+		vertices,						//all vertices
+		GL_STATIC_DRAW					//how the data will update
 	);
 
 	//Enable vertex position as first attribute
@@ -70,17 +57,27 @@ void Mesh::start()
 		0						//memory position of this attribute
 	);
 
+	//Enable vertex color as second attribute
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(
+		1,						//attribute index
+		4,						//number of values within attribute
+		GL_FLOAT,				//type of each value
+		GL_FALSE,				//whether to normalize
+		sizeof(Vertex),			//size in bytes of one vertex
+		(void*)sizeof(glm::vec4)//memory position of this attribute
+	);
+
 	//Unbind buffer and array
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//Deallocate vertices
+	delete[] vertices;
 }
 
 void Mesh::draw()
 {
 	glBindVertexArray(m_vertexArrayObject);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-}
-
-void Mesh::generateVertices(Vertex* vertices, int& vertexCount)
-{
+	glDrawArrays(GL_TRIANGLES, 0, m_triCount * 3);
 }
