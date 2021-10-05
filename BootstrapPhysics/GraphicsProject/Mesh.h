375e57820e4ff77bc3f2e5b66983410e144241af
Ben Brownlie <benjibspam@gmail.com>
2021-10-05 12:18:31 -0500

    Added vertex normals

--- BootstrapPhysics/GraphicsProject/Mesh.h
+++ BootstrapPhysics/GraphicsProject/Mesh.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Shader.h"
 #include "gl_core_4_4.h"
 #include "glm\vec4.hpp"
 #include "glm/mat4x4.hpp"
@@ -10,6 +11,7 @@ public:
 	{
 		glm::vec4 position;
 		glm::vec4 color;
+		glm::vec4 normal;
 	};
 
 public:
@@ -17,7 +19,7 @@ public:
 	~Mesh();
 
 	void start();
-	void draw();
+	void draw(aie::ShaderProgram* shader);
 
 	virtual Vertex* generateVertices(unsigned int& vertexCount, unsigned int& triCount) = 0;
 
